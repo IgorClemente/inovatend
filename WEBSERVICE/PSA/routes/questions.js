066763f6e46f810ced93ff36d70db6bed5149a35
Igor Clemente <igor.clemente@outlook.com>
2018-09-24 17:09:04 -0300

    _QUESTIONSETRESPONSE_UPDATE_24092018

--- WEBSERVICE/PSA/routes/questions.js
+++ WEBSERVICE/PSA/routes/questions.js
@@ -601,11 +601,11 @@ router.post('/response/:questionID', function(req,res,next) {
            const questionResultIdentifier = results[0].questionIdentifier;
            const questionResultResponseIdentifier = results[0].questionResponseIdentifier;
 
-           if (questionIdentifierParam == questionResultIdentifier) {
-               if (questionResponseIdentifier == questionResultResponseIdentifier) {
+           if (!(questionIdentifierParam == questionResultIdentifier)) {
+               if (!(questionResponseIdentifier == questionResultResponseIdentifier)) {
                    res.json({
-                       'success' : true,
-                       'successMessage' : 'Resposta adicionada com sucesso.'
+                       'success' : false,
+                       'errorMessage' : 'Erro ao verificar resposta para questão.'
                    });
                    connection.release();
                    return;
@@ -613,8 +613,8 @@ router.post('/response/:questionID', function(req,res,next) {
            }
 
            res.json({
-               'success' : false,
-               'errorMessage' : 'Erro ao verificar resposta para questão.'
+               'success' : true,
+               'successMessage' : 'Resposta adicionada com sucesso.'
            });
            connection.release();
        });
